9f550157446c269c82cf3958ff541309cc4d4679
Diego Hdez <di_hernandez@uxmcc2.iimas.unam.mx>
2020-12-17 12:55:39 -0600

    Added website, seeking_venue and seeking_description columns to Artist model

--- app.py
+++ app.py
@@ -50,8 +50,12 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
+    website = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(120))
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
